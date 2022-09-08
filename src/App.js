260a4c08812a8b7367a665e2d10450901df6ca71
Scientist <s.gamer3327@gmail.com>
2022-09-09 00:53:23 +0400

    categories section was added

--- src/App.js
+++ src/App.js
@@ -10,8 +10,24 @@ function App() {
         
         <div style={{width:"100%"}}>
           <Header />
-          <nav className="row mr-2 navbar navbar-white bg-white" style={{borderBottom: "2px solid #e5e5e5"}}>
-            <button className="btn btn-lg bg-light">Music</button>
+          <nav className="row mr-2 navbar navbar-white bg-white categories" style={{borderBottom: "2px solid #e5e5e5", justifyContent:"flex-start"}}>
+            <button className="btn btn-lg py-1 px-3 mx-3 text-white" style={{background:"black" ,fontSize: "17px", borderRadius:"500px"}}> All </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            <button className="btn btn-lg py-1 px-3  mr-3 " style={{fontSize: "17px", borderRadius:"500px"}}> Music </button>
+            
+            
           </nav>
           <VideoList />
         </div>
